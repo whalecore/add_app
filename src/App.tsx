0cb36151965e103d1922baa322c0bfe4a954849c
whalecore <whal3core@gmail.com>
2021-11-26 20:00:23 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,7 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-import Header from "./components/header/header";
-import HomePage from "./pages/homepage/homepage";
-import AddPage from "./pages/add/add-page";
-import LoginPage from "./pages/login/login-page";
-
-import { userStore } from "./store/user";
 
 const App = (): JSX.Element => {
-  return (
-    <Router>
-      <div>
-        <Header currentUser={userStore.user.loggedIn ? true : false} />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/add" component={AddPage} />
-          <Route exact path="/login" component={LoginPage} />
-        </Switch>
-      </div>
-    </Router>
-  );
+  return <div></div>;
 };
 
 export default App;
